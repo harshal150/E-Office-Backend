dd1f352ecd8aa158bfddc1d07a93ddcd25efe97d
harshal <harshhh30601@gmail.com>
2024-06-24 14:00:59 +0530

    updated

--- routes/Meetings/MeetingsRoutes.js
+++ routes/Meetings/MeetingsRoutes.js
@@ -1,15 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// const meetingsController = require('../../controllers/Meetings/MeetingsController');
-
-// router.post('/addMeeting', meetingsController.addMeeting);
-// router.get('/meetings-by-participant', meetingsController.getMeetingsByParticipantId);
-
-// module.exports = router;
-
-
-
-
 
 // routes/meetingRoutes.js
 const express = require('express');
